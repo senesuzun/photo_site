aac53e49bd1afcb364cb7117720c641d618eeaba
senesuzun <senesuzun@gmail.com>
2023-03-08 17:46:08 +0100

    Initial commit

---
+++ src/main/java/be/thomasmore/photo_site/PhotoSiteApplication.java
@@ -0,0 +1,13 @@
+package be.thomasmore.photo_site;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoSiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhotoSiteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/be/thomasmore/photo_site/PhotoSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package be.thomasmore.photo_site;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoSiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
